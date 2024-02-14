11ac403ac218cbc104d0efb366b298ede91c228d
jithinsankar <jithinsankar.nk@gmail.com>
2024-02-15 00:51:23 +0530

    tracking

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from "path"
-
+import { VitePluginRadar } from 'vite-plugin-radar'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePluginRadar({
+    // Google Analytics tag injection
+    analytics: {
+      id: 'G-XXXXXXX',
+    },
+  })],
   base:"/body_fat_percentage/",
   resolve: {
     alias: {
